95ee4d374478d7855f561d7e9d5337c710d31ec9
mizuki-s-47 <91833469+mizuki-s-47@users.noreply.github.com>
2021-10-03 22:42:32 +0900

    初めてのWebサイト

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/icon1.png') {
+      myImage.setAttribute('src','images/icon777.png');
+    } else {
+      myImage.setAttribute('src','images/icon1.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+    let myName=prompt('Please enter your name.');
+    if(!myName){
+        setUserName();
+    }else{
+        localStorage.setItem('name',myName);
+        myHeading.innerHTML='Welcome to my page, ' + myName + '!';
+    }
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+}else{
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML='Welcome to my page, ' + storedName + '!';
+}
+
+myButton.onclick=function(){
+    setUserName();
+}
